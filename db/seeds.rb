bcaee6ee3087dc543c9b1b75ffd15e9bbd25dfb6
Nadia Gaggini <nadia.gaggini@gmail.com>
2026-03-04 14:09:15 +0100

    routes changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+require "json"
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Fetching top rated movies from TMDB proxy..."
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+json = URI.open(url).read
+data = JSON.parse(json)
+
+data["results"].first(20).each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Done! Created #{Movie.count} movies."
